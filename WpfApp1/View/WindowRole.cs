03089158536dde1712e3eca0b44beff885388e47
Your Name <bogdandynay@gmail.com>
2022-05-02 18:16:28 +0300

    first work

---
+++ WpfApp1/View/WindowRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace WpfApplDemo2018.Views
+{
+    public partial class WindowRole : Window
+    {
+        public WindowRole()
+        {
+            InitializeComponent();
+        }
+    }
+}
